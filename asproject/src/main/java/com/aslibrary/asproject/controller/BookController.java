e1e74a842898cc462826f3c0cd571911f5f8b668
CamiloEMG <cemartinez82@ucatolica.edu.co>
2024-03-06 12:45:57 -0500

    endpoint create book

--- asproject/src/main/java/com/aslibrary/asproject/controller/BookController.java
+++ asproject/src/main/java/com/aslibrary/asproject/controller/BookController.java
@@ -2,9 +2,14 @@ package com.aslibrary.asproject.controller;
 
 import com.aslibrary.asproject.entities.Book;
 import com.aslibrary.asproject.entities.Country;
+import com.aslibrary.asproject.repositories.BookRepository;
 import com.aslibrary.asproject.services.BookService;
 import com.aslibrary.asproject.services.CountryService;
+import com.aslibrary.asproject.services.CustomerService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -34,5 +39,22 @@ public class BookController {
         return bookService.findBookByTitle(title);
     }
 
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    @ResponseBody
+    public ResponseEntity<String> createBook(@Valid @RequestBody Book book) {
+        try {
+            bookRepository.save(book);
+            return new ResponseEntity<>("{\"message\": \"Libro creado exitosamente\"}", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>("{\"error\": \"Error al crear el libro: " + e.getMessage() + "\"}", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 }
