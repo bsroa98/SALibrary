d29b12ae64956c81131f676c1e471e5234345796
JuanBocanegraUCC <jjbocanegra99@ucatolica.edu.co>
2024-04-17 12:24:22 -0500

    Primer borrador front

--- Front/library-front/src/App.js
+++ Front/library-front/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button className='btn btn-primary'> CREAR </button>
-      </header>
+    <div className="body">
+      <div className="header">
+        <a href="/">Home</a>
+        <a href="/">Shop</a>
+        <input type="text" placeholder="Search" />
+        <a href="/">Sign Up</a>
+        <a href="/">Sign In</a>
+      </div>
+
+      <div className="contenido">
+        <section className="product">
+          <div className="productcard">
+            <img src="../imgs/Dracula.jpg" alt="accesorio" />
+            <h3>Accesorios</h3>
+            <p>Precio: $50.000</p>
+            <button className="button">Dracula</button>
+          </div>
+          <div className="productcard">
+            <img src="../imgs/LaDivinaComedia.jpg" alt="ropa" />
+            <h3>Ropa</h3>
+            <p>Precio: $60.000</p>
+            <button className="button">La Divina Comedia</button>
+          </div>
+          <div className="productcard">
+            <img src="../imgs/Dracula.jpg" alt="zapato" />
+            <h3>Zapatos</h3>
+            <p>Precio: $70.000</p>
+            <button className="button">Dracula</button>
+          </div>
+          <div className="productcard">
+            <img src="../imgs/LaDivinaComedia.jpg" alt="perfume" />
+            <h3>Perfume</h3>
+            <p>Precio: $80.000</p>
+            <button className="button">La Divina Comedia</button>
+          </div>
+        </section>
+      </div>
+
+      <footer>
+        <p></p>
+      </footer>
     </div>
   );
 }
